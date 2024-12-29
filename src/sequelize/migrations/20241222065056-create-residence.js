da2f0bd9da5d6f97e2fce802b020efa9db32f807
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2024-12-30 01:36:58 +0900

    execute format

--- src/sequelize/migrations/20241222065056-create-residence.js
+++ src/sequelize/migrations/20241222065056-create-residence.js
@@ -26,7 +26,7 @@ module.exports = {
       },
       constructed_at: {
         type: Sequelize.DATE,
-      }
+      },
     });
     await queryInterface.addIndex('residences', ['address'], { unique: true });
   },
