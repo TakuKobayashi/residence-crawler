92af8413b812be9d128b0bdcec8fab2ece2aabca
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2025-01-03 03:31:15 +0900

    賃貸エリアのリストを取得できるようにした

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,7 @@
 import { program, Command } from 'commander';
 import packageJson from '../package.json';
+import nodeHtmlParser from 'node-html-parser';
+import axios from 'axios';
 
 import { config } from 'dotenv';
 config();
@@ -19,6 +21,24 @@ crawlCommand
   .description('')
   .action(async (options: any) => {
     console.log('suumo crawler');
+  });
+
+crawlCommand
+  .command('suumo:rooturl')
+  .description('')
+  .action(async (options: any) => {
+    const searchUrl = new URL('https://suumo.jp/chintai/tokyo/');
+    const response = await axios.get(searchUrl.toString());
+    const root = nodeHtmlParser.parse(response.data.toString());
+    const itemDoms = root.querySelectorAll('ul.itemtoplist');
+    for (const itemDom of itemDoms) {
+      const itemAtagDoms = itemDom.querySelectorAll('a');
+      for(const itemAtagDom of itemAtagDoms) {
+        const aTagAttrs = itemAtagDom.attrs || {};
+        console.log(aTagAttrs);
+        console.log(itemAtagDom.text)
+      }
+    }
   });
 
 program.addCommand(crawlCommand);
