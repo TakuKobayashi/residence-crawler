553a891445b1638dcf849a3b03dbe76e9c6404e9
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2025-01-19 15:24:11 +0900

    量が多いSQLのINSERTにも対応した実装

--- src/libs/utils/data-importers.ts
+++ src/libs/utils/data-importers.ts
@@ -6,15 +6,27 @@ import fg from 'fast-glob';
 import readline from 'readline';
 
 export async function importFromSqls() {
+  const loggingStatus = models.sequelize.options.logging;
   const appDir = path.dirname(require.main?.filename || '');
   const sqlFilePathes = fg.sync([...appDir.split(path.sep), `..`, 'data', 'sqls', `tables`, '**', '*.sql'].join('/'), { dot: true });
   for (const sqlFilePath of sqlFilePathes) {
     await new Promise<void>((resolve, reject) => {
+      // 量が多いのでSQLは表示しない
+      models.sequelize.options.logging = false;
       const executeSqlPromises: Promise<number>[] = [];
       const insertSqlFileStream = fs.createReadStream(sqlFilePath);
       const reader = readline.createInterface({ input: insertSqlFileStream });
       reader.on('line', async (insertSql) => {
-        executeSqlPromises.push(models.sequelize.query(insertSql));
+        if (insertSql.length > 0) {
+          executeSqlPromises.push(models.sequelize.query(insertSql));
+          // 非同期でINSERTを実行しすぎると処理しきれなくなるので途中でいったん止めて同期する
+          if (executeSqlPromises.length % 100 === 0) {
+            reader.pause();
+            await Promise.all(executeSqlPromises);
+            executeSqlPromises.splice(0);
+            reader.resume();
+          }
+        }
       });
       reader.on('close', async () => {
         await Promise.all(executeSqlPromises);
@@ -22,4 +34,5 @@ export async function importFromSqls() {
       });
     });
   }
+  models.sequelize.options.logging = loggingStatus;
 }
