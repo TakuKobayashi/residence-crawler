9f543ad311d7a265179a346bc93fc0150baf26b0
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2025-02-05 23:13:44 +0900

    1日に1回にクローラーが稼働するようにした

--- src/cron.ts
+++ src/cron.ts
@@ -1,17 +1,15 @@
 import { CronJob } from 'cron';
+import { crawlPropertyInfos } from './libs/crawlers/suumo';
+import { exportToCSV } from './libs/utils/data-exporters';
 
 var job = CronJob.from({
   //実行したい日時 or crontab書式
-  cronTime: '* * * * *',
+  cronTime: '* * 16 * *',
 
   //指定時に実行したい関数
-  onTick: () => {
-    console.log('onTick!');
-  },
-
-  //ジョブの完了または停止時に実行する関数
-  onComplete: () => {
-    console.log('onComplete!');
+  onTick: async () => {
+    await crawlPropertyInfos();
+    await exportToCSV();
   },
 
   // コンストラクタを終了する前にジョブを開始するかどうか
